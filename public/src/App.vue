2aebd38dae6dd48886264a5bc578fb80d45524de
taoxin <taoxin167@gmail.com>
2017-02-10 22:02:36 +0800

    调整框架

--- public/src/App.vue
+++ public/src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="app">
+    <upload></upload>
   </div>
 </template>
 
 <script>
+  import upload from './index.vue'
   export default {
     components: {
+      upload
     },
     data () {
       return {
@@ -20,23 +23,10 @@
   }
 
   body {
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    /*display: flex;
+    align-items: center;*/
     height: 100%;
   }
 
-  #app {
-    color: #2c3e50;
-    font-family: Source Sans Pro, Helvetica, sans-serif;
-    width:100%; 
-    margin-top: 100px;
-    padding-left: 20px;
-  }
-
-  #app a {
-    color: #42b983;
-    text-decoration: none;
-  }
 
 </style>
