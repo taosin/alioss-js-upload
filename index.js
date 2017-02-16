b8b8663a9137fe99639cfe87feba334ff24cf14d
taoxin <taoxin167@gmail.com>
2017-02-16 22:59:14 +0800

    解决跨域问题

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ var port = "8188"
 var app = express()
 var service = require('./service.js')
 
-setCrossDomain (req, res, next) {
-  res.append('Access-Control-Allow-Origin', '*')
-  res.append('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, If-Modified-Since')
-  res.append('Access-Control-Allow-Credentials', 'true')
-  res.append('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE')
-  res.append('X-Powered-By', '3.2.1')
-  res.append('Content-Type', 'application/json;charset=utf-8')
-  next()
-}
+// setCrossDomain (req, res, next) {
+//   res.append('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, If-Modified-Since')
+//   res.append('Access-Control-Allow-Credentials', 'true')
+//   res.append('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE')
+//   res.append('X-Powered-By', '3.2.1')
+//   res.append('Content-Type', 'application/json;charset=utf-8')
+//   next()
+// }
+// app.use(setCrossDomain)
 module.exports = app.listen(port, function (err) {
   if (err) {
     console.log(err)
@@ -23,6 +23,7 @@ module.exports = app.listen(port, function (err) {
   }
   // 获取token
   app.get('/alioss/getOssToken', function(req, res) {
+    res.append('Access-Control-Allow-Origin', '*')
     const result = service.getOssToken(req, res)
     if (result) {
       res.json({
@@ -34,4 +35,3 @@ module.exports = app.listen(port, function (err) {
 
   console.log('Listening at http://localhost:' + port + '\n')
 })
-app.use(setCrossDomain)
