7e9fa1e6676d4624aa601604bb7c3b6a62b469a2
iMocco <taoxin167@gmail.com>
2017-08-08 11:41:44 +0800

    提交断点上传

--- public/src/components/upload.vue
+++ public/src/components/upload.vue
@@ -2,29 +2,31 @@
   <div class="index">
     <div class="oss_file">
       <input type="file" :id="id" @change="doUpload"/>
+      {{percentage}}
     </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'hello',
+    name: 'upload',
     data () {
       return {
-        region: 'oss-cn-shanghai',
+        region: 'oss-cn-hangzhou',
         bucket: 'aliyun-oss-upload',
-        id: 'upload'
+        id: 'upload',
+        percentage: 0
       }
     },
     methods:{
       doUpload () {
         const _this = this
-        Vue.axios.get('/alioss/getOssToken').then((result) => {
+        _this.axios.get('/alioss/getOssToken').then((result) => {
           const client = new OSS.Wrapper({
             region: _this.region,
-            accessKeyId: result.token.AccessKeyId,
-            accessKeySecret: result.token.AccessKeySecret,
-            stsToken: result.token.SecurityToken,
+            accessKeyId: result.data.token.AccessKeyId,
+            accessKeySecret: result.data.token.AccessKeySecret,
+            stsToken: result.data.token.SecurityToken,
             bucket: _this.bucket
           })
           const files = document.getElementById(_this.id)
@@ -33,19 +35,15 @@
             let resultUpload = ''
             for (let i = 0; i < fileLen.length; i++) {
               const file = fileLen[i]
-              const storeAs = file.name
-              if (file.size / 1000 > 1000) {
-                _this.$Message.warning('图片大小不能超过1M')
-                return
-              }
-              client.multipartUpload(storeAs, file, {
-              }).then((results) => {
-                if (results) {
-                  const url = 'http:/' + results.name
-                  _this.Imgurl = url
-                } else {
-                  this.$Message().error('上传出错啦，再试试！')
+              let random_name = this.random_string(6) + '_' + new Date().getTime() + '.' + file.name.split('.').pop()
+              client.multipartUpload(random_name, file, {
+                progress: function* (percentage, cpt) {
+                  // 上传进度
+                  _this.percentage = percentage
                 }
+              }).then((results) => {
+                // 上传完成
+                console.log('http://oss-upload.oss-cn-hangzhou.aliyuncs.com/'+ results.name);
               }).catch((err) => {
                 console.log(err)
               })
@@ -53,6 +51,16 @@
           }
         })
       },
+      random_string(len) {
+        　　len = len || 32;
+        　　var chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';   
+        　　var maxPos = chars.length;
+        　　var pwd = '';
+        　　for (let i = 0; i < len; i++) {
+          　　pwd += chars.charAt(Math.floor(Math.random() * maxPos));
+        }
+        return pwd;
+      }
     }
   }
 </script>
--- public/src/main.js
+++ public/src/main.js
@@ -4,6 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import axios from 'axios'
+
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+
+Vue.axios.defaults.baseURL = 'http://localhost:8188'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
